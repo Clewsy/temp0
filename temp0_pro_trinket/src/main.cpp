6325b0b2c79d801da0b5fbccdde767990067344f
clewsy <jadon@clews.pro>
2020-03-28 17:40:34 +1100

    Added larger font and second display mode.

--- temp0_pro_trinket/src/main.cpp
+++ temp0_pro_trinket/src/main.cpp
@@ -38,25 +38,21 @@ void update_oled (double temp, double humi)
 	switch(mode)
 	{
 		case MODE_NORMAL:
-			display.print_large_string((unsigned char *)"Temperature", 0, 0);
-			display.print_large_double(temp, 2, 0);
-			display.print_large_char(176, 2, 39);
-			display.print_large_char('C', 2, 47);
+			display.print_string((unsigned char *)"Temperature", 0, 0);
+			display.print_double(temp, 2, 0);
+			display.print_char(176, 2, 39);
+			display.print_char('C', 2, 47);
 
-			display.print_large_double(humi, 4, 79);
-			display.print_large_char('%', 4, 119);
-			display.print_large_string((unsigned char *)"Humidity", 6, 63);
+			display.print_double(humi, 4, 79);
+			display.print_char('%', 4, 119);
+			display.print_string((unsigned char *)"Humidity", 6, 63);
 			break;
 
 		case MODE_WEIRD:
-			display.print_large_string((unsigned char *)"Temperature", 0, 0);
-			display.print_large_double(temp, 2, 0);
-			display.print_large_char(176, 2, 39);
-			display.print_large_char('C', 2, 47);
-
+			display.print_large_double(temp, 0, 0);
+			display.print_large_char(LARGE_CHAR_INDEX_DEG, 0, 96);
 			display.print_large_double(humi, 4, 0);
-			display.print_large_char('%', 4, 40);
-			display.print_large_string((unsigned char *)"Humidity", 6, 0);
+			display.print_large_char('%', 4, 96);
 			break;
 	}
 }
@@ -90,7 +86,7 @@ void setup(void) {
 	display.draw_box(0, 0, 8, 128);
 	display.draw_box(1, 16, 6, 96);
 	display.draw_box(2, 32, 4, 64);
-	display.print_large_string((unsigned char *) "temp0", 3, 43);
+	display.print_string((unsigned char *) "temp0", 3, 43);
 	//////////////// End Splash Screen Animation
 
 	// Run heater while the splash screen is displayed.
--- temp0_pro_trinket/src/ssd1306.cpp
+++ temp0_pro_trinket/src/ssd1306.cpp
@@ -115,34 +115,69 @@ void ssd1306::init(void)
 	send_command(OLED_ON);									//Turn display on.
 }
 
-void ssd1306::print_large_char(unsigned char character, uint8_t start_page, uint8_t start_column)
+void ssd1306::print_char(unsigned char character, uint8_t start_page, uint8_t start_column)
 {
 	for(uint8_t p=0; p<2; p++)							//Characters span two pages.
 	{
 		set_address((start_page+p), start_column);
-		for(uint8_t i=(0+(8*p)); i<(8+(8*p)); i++)				//Cycle through each of the 8 segments on the current character page.
+		for(uint8_t i=(8*p); i<(8+(8*p)); i++)					//Cycle through each of the 8 segments on the current character page.
 		{
 			send_data(pgm_read_byte(&(font_8x16[character-32][i])));	//Send the data that corresponds to the segments defined in the font.
 		}									//Note "-32" is required to map the decimal value of <char> to the defined font.
 	}
 }
 
+void ssd1306::print_string(unsigned char *string, uint8_t start_page, uint8_t start_column)
+{
+	uint8_t c = 0;
+	while(string[c])
+	{
+		print_char(string[c], start_page, (start_column+(c*8)));
+		c++;
+	} 
+}
+
+void ssd1306::print_double(double number, uint8_t start_page, uint8_t start_column)
+{
+	char buffer[5];
+	dtostrf(number, 4, 2, buffer);			//Double-to-String function.  Syntax dtostrf(double, min_string_length, num_post_decimal_values, char_array_addr).
+
+	print_string((unsigned char *) buffer, start_page, start_column);
+}
+
+
+void ssd1306::print_large_char(unsigned char character, uint8_t start_page, uint8_t start_column)
+{
+	for(uint8_t p=0; p<4; p++)							//Characters span four pages.
+	{
+		set_address((start_page+p), start_column);
+		for(uint8_t i=(24*p); i<(24+(24*p)); i++)				//Cycle through each of the 8 segments on the current character page.
+		{
+			send_data(pgm_read_byte(&(font_24x32[character-32][i])));	//Send the data that corresponds to the segments defined in the font.
+		}									//Note "-32" is required to map the decimal value of <char> to the defined font.
+	}
+}
 void ssd1306::print_large_string(unsigned char *string, uint8_t start_page, uint8_t start_column)
 {
 	uint8_t c = 0;
 	while(string[c])
 	{
-		print_large_char(string[c], start_page, (start_column+(c*8)));
+		print_large_char(string[c], start_page, (start_column+(c*24)));
 		c++;
 	} 
 }
 
 void ssd1306::print_large_double(double number, uint8_t start_page, uint8_t start_column)
 {
-	char buffer[5];
-	dtostrf(number, 4, 2, buffer);	//Double-to-String function.  Syntax dtostrf(double, min_string_length, num_post_decimal_values, char_array_addr).
+	char buffer[4];					//Clip the last digit (hudredths).
+	dtostrf(number, 4, 1, buffer);			//Double-to-String function.  Syntax dtostrf(double, min_string_length, num_post_decimal_values, char_array_addr).
+
 	print_large_string((unsigned char *) buffer, start_page, start_column);
 }
+
+
+
+
 
 void ssd1306::test_pattern(void)
 {
