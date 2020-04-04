f9586df7025288cd9fba44934bbc0052671992e6
clewsy <jadon@clews.pro>
2020-04-04 13:51:57 +1100

    Tidied comments.

--- temp0_pro_trinket/src/ssd1306.cpp
+++ temp0_pro_trinket/src/ssd1306.cpp
@@ -1,10 +1,12 @@
 #include "ssd1306.h"
 
+//Initiate an oled module.
 ssd1306::ssd1306(void)
 {
-	Wire.begin();	//Communicating with the ssd1306 requires I2C.
+	//Nothing needed here.
 }
 
+//Send a command to oled driver - single command only.
 void ssd1306::send_command(uint8_t command)
 {
 	Wire.beginTransmission(OLED_ADDR);
@@ -13,6 +15,7 @@ void ssd1306::send_command(uint8_t command)
 	Wire.endTransmission(OLED_ADDR);
 }
 
+//Send a command to oled driver - single command followed by command value.
 void ssd1306::send_command(uint8_t command, uint8_t value)
 {
 	Wire.beginTransmission(OLED_ADDR);
@@ -52,6 +55,7 @@ void ssd1306::set_address(uint8_t page, uint8_t column)
 	set_column(column);
 }
 
+//Write all zeros to oled data to clear all pixels.
 void ssd1306::clear_screen(void)
 {
 	for (uint8_t page=0; page<8; page++)		//Cycle through all 8 pages.
@@ -64,9 +68,11 @@ void ssd1306::clear_screen(void)
 	}
 }
 
+//Draw a box with top left corner at upper pixel of [start page] and column [start_column],
+//with dimensions [width] pixels wide by [height]x8 pixels height (i.e. height is in incrments of page = 8-bits).
 void ssd1306::draw_box(uint8_t start_page, uint8_t start_column, uint8_t height, uint8_t width)
 {
-	set_address(start_page, start_column);
+	set_address(start_page, start_column);					//Location of top left box corner.
 
 	send_data(0xFF);							//Left most column, first page. 
 
@@ -97,25 +103,27 @@ void ssd1306::draw_box(uint8_t start_page, uint8_t start_column, uint8_t height,
 	}
 }
 
+//Initialise the oled display.
 void ssd1306::init(void)
 {
-	send_command(OLED_SET_MULTIPLEX_RATIO, OLED_DEFAULT_MULTIPLEX_RATIO);			//Set MUX ratio.
-	send_command(OLED_SET_DISPLAY_OFFSET, OLED_DEFAULT_DISPLAY_OFFSET);			//Set display offset.
-	send_command(OLED_SET_DISPLAY_START_LINE);						//Set display start line.
-	send_command(OLED_SET_SEGMENT_REMAP_COL0);						//Set segment re-map.
-	send_command(OLED_SET_COM_OUTPUT_SCAN_DIR_NORMAL);					//Set COM output scan direction.
-	send_command(OLED_SET_COM_PINS_HARDWARE_CONFIG, OLED_DEFAULT_COM_PINS_HARDWARE_CONFIG);	//Set COM pins hardware configuration.
-	send_command(OLED_CONTRAST, OLED_DEFAULT_CONTRAST);					//Set contrast control.
-	send_command(OLED_ALL_ON_RESUME);							//Disable entire display on.
-	send_command(OLED_INVERSE_DISABLE);							//Set Normal display (i.e. not inverse).
-	send_command(OLED_SET_DISPLAY_CLOCK, OLED_DEFAULT_DISPLAY_CLOCK);			//Set Osc frequency.
-	send_command(OLED_SET_CHARGE_PUMP, OLED_DEFAULT_SET_CHARGE_PUMP);			//Enable charge pump regulator.
-	send_command(OLED_SET_PRECHARGE_PERIOD, OLED_DEFAULT_PRECHARGE_PERIOD);			//Set charge pump pre-charge period.
-	send_command(OLED_SET_MEMORY_ADDRESSING_MODE, OLED_DEFAULT_MEMORY_ADDRESSING_MODE);	//Set memory addressing mode to Page Addressing Mode.
-	clear_screen();										//Clear any remnant screen memory from last run.
-	send_command(OLED_ON);									//Turn display on.
+	send_command(OLED_SET_MULTIPLEX_RATIO, OLED_SET_MULTIPLEX_RATIO_DEFAULT);			//Set MUX ratio.
+	send_command(OLED_SET_DISPLAY_OFFSET, OLED_SET_DISPLAY_OFFSET_DEFAULT);				//Set display offset.
+	send_command(OLED_SET_DISPLAY_START_LINE);							//Set display start line.
+	send_command(OLED_SET_SEGMENT_REMAP_COL0);							//Set segment re-map.
+	send_command(OLED_SET_COM_OUTPUT_SCAN_DIR_NORMAL);						//Set COM output scan direction.
+	send_command(OLED_SET_COM_PINS_HARDWARE_CONFIG, OLED_SET_COM_PINS_HARDWARE_CONFIG_DEFAULT);	//Set COM pins hardware configuration.
+	send_command(OLED_SET_CONTRAST, OLED_SET_CONTRAST_DEFAULT);					//Set contrast control.
+	send_command(OLED_ALL_ON_RESUME);								//Disable entire display on.
+	send_command(OLED_INVERSE_DISABLE);								//Set Normal display (i.e. not inverse).
+	send_command(OLED_SET_DISPLAY_CLOCK, OLED_SET_DISPLAY_CLOCK_DEFAULT);				//Set Osc frequency.
+	send_command(OLED_SET_CHARGE_PUMP, OLED_SET_CHARGE_PUMP_DEFAULT);				//Enable charge pump regulator.
+	send_command(OLED_SET_PRECHARGE_PERIOD, OLED_SET_PRECHARGE_PERIOD_DEFAULT);			//Set charge pump pre-charge period.
+	send_command(OLED_SET_MEMORY_ADDRESSING_MODE, OLED_SET_MEMORY_ADDRESSING_MODE_DEFAULT);		//Set memory addressing mode to Page Addressing Mode.
+	clear_screen();											//Clear any remnant screen memory from last run.
+	send_command(OLED_ON);										//Turn display on.
 }
 
+//Print a single character using an 8x16 pixel font.
 void ssd1306::print_char(unsigned char character, uint8_t start_page, uint8_t start_column)
 {
 	for(uint8_t p=0; p<2; p++)							//Characters span two pages.
@@ -128,16 +136,18 @@ void ssd1306::print_char(unsigned char character, uint8_t start_page, uint8_t st
 	}
 }
 
+//Print a string of characters using an 8x16 pixel font.
 void ssd1306::print_string(unsigned char *string, uint8_t start_page, uint8_t start_column)
 {
 	uint8_t c = 0;
-	while(string[c])
+	while(string[c])	//Print each character until end of string.
 	{
 		print_char(string[c], start_page, (start_column+(c*8)));
 		c++;
 	} 
 }
 
+//Convert a double to a character string and print in an 8x16 pixel font.
 void ssd1306::print_double(double number, uint8_t start_page, uint8_t start_column)
 {
 	char buffer[5];
@@ -146,7 +156,7 @@ void ssd1306::print_double(double number, uint8_t start_page, uint8_t start_colu
 	print_string((unsigned char *) buffer, start_page, start_column);
 }
 
-
+//Print a single character using a 24x32 pixel font.
 void ssd1306::print_large_char(unsigned char character, uint8_t start_page, uint8_t start_column)
 {
 	for(uint8_t p=0; p<4; p++)							//Characters span four pages.
@@ -159,16 +169,18 @@ void ssd1306::print_large_char(unsigned char character, uint8_t start_page, uint
 	}
 }
 
+//Print a string of characters using a 24x32 pixel font.
 void ssd1306::print_large_string(unsigned char *string, uint8_t start_page, uint8_t start_column)
 {
 	uint8_t c = 0;
-	while(string[c])
+	while(string[c])	//Print each character until end of string.
 	{
 		print_large_char(string[c], start_page, (start_column+(c*24)));
 		c++;
 	} 
 }
 
+//Convert a double to a character string and print in a 24x32 pixel font.
 void ssd1306::print_large_double(double number, uint8_t start_page, uint8_t start_column)
 {
 	char buffer[5];					//Clip the last digit (hudredths).
@@ -177,6 +189,7 @@ void ssd1306::print_large_double(double number, uint8_t start_page, uint8_t star
 	print_large_string((unsigned char *) buffer, start_page, start_column);
 }
 
+//Print a test patter to the display.
 void ssd1306::test_pattern(void)
 {
 	for (uint8_t page=0;page<8;page++)
@@ -186,7 +199,7 @@ void ssd1306::test_pattern(void)
 		{
 			send_command((col&0b1111), ((col>>4)+0b10000));
 			set_address(page, col);
-			send_data(0b00001111);
+			send_data(0b11110000);
 		}
 	}
 }
